5bd51830b2fcbd6d098f6d1f4a5e97c85c975a16
Leo <80970758+LeonardoRyuta@users.noreply.github.com>
2025-02-09 09:55:41 +0000

    cors fix attempt 2

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
 from routes import ideas,scoring,nft
+from fastapi.middleware.cors import CORSMiddleware
 
 # Initialize FastAPI app
 app = FastAPI(
     title="Idea Marketplace API",
     description="An API for submitting and ""patenting"" ideas as NFTs",
     version="1.0"
+)
+
+origins = ["http://127.0.0.1:5173", "http://localhost:5173"]
+
+app.add_middleware(
+
+    CORSMiddleware,
+
+    allow_origins=origins,
+
+    allow_credentials=True,
+
+    allow_methods=["*"],
+
+    allow_headers=["*"],
+
 )
 
 # Include API routes
